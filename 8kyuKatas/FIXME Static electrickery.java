2b4bb24dac638aa06359ab9f5957d6b41ec215dc
WiktorG3 <wiktor.gronostaj03@gmail.com>
2024-10-09 17:19:02 +0200

    Add files via upload

---
+++ 8kyuKatas/FIXME Static electrickery.java
@@ -0,0 +1,17 @@
+public class Dinglemouse {
+
+  public static final Dinglemouse INST = new Dinglemouse();
+      
+  private static final int ONE_HUNDRED = 100;
+
+  private final int value; 
+
+  private Dinglemouse() {
+    value = ONE_HUNDRED;
+  }
+  
+  public int plus100(int n) {
+    return value + n;
+  }
+  
+}
