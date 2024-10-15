4be45068b8293b3770dcb8a68a70eb3009f37053
WiktorG3 <wiktor.gronostaj03@gmail.com>
2024-10-15 12:30:32 +0200

    You Can't Code Under Pressure

---
+++ 8kyuKatas/You Can't Code Under Pressure #1.java
@@ -0,0 +1,9 @@
+/* Description:
+Code as fast as you can! You need to double the integer and return it.
+*/
+class Java {
+  public static int doubleInteger(int i) {
+    // Double the integer and return it!
+    return i*2;
+  }
+}
