44d87fb9e4182469f003b50fb522d7e94e0d6146
Wiktor Gronostaj <wiktor.gronostaj03@gmail.com>
2024-10-21 14:35:43 +0200

    Update Opposites Attract.java

--- 8kyuKatas/Opposites Attract.java
+++ 8kyuKatas/Opposites Attract.java
@@ -5,6 +5,6 @@ Write a function that will take the number of petals of each flower and return t
 
 public class OppositesAttract {
   public static boolean isLove(final int flower1, final int flower2) {
-    return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0);
+    return flower1 % 2 != flower2 % 2;
   }
 }
