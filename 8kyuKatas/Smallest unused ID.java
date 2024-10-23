b5a74bdc4cc517236042c6a9464fa68c0cc9576a
Wiktor Gronostaj <wiktor.gronostaj03@gmail.com>
2024-10-23 14:49:44 +0200

    Smallest unused ID

---
+++ 8kyuKatas/Smallest unused ID.java
@@ -0,0 +1,24 @@
+/* Description:
+Hey awesome programmer!
+You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+Therefore you need a method, which returns the smallest unused ID for your next new data item...
+Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+Go on and code some pure awesomeness!
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+    public static int nextId(int[] ids) {
+      Arrays.sort(ids);
+      int smallestUnusedId = 0;
+      
+      for (int id : ids) {
+        if (id == smallestUnusedId) {
+          smallestUnusedId++;
+        }
+      }
+      
+      return smallestUnusedId;
+    }
+}
