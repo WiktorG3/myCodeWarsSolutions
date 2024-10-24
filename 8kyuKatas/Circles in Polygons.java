9b3c75266b006b828574f7a590fd69a950b974c5
Wiktor Gronostaj <wiktor.gronostaj03@gmail.com>
2024-10-24 15:55:02 +0200

    Circles in Polygons

---
+++ 8kyuKatas/Circles in Polygons.java
@@ -0,0 +1,24 @@
+/* Description:
+You are the owner of a box making company.
+Your company can produce any equal sided polygon box, but plenty of your customers want to transport circular objects in these boxes. Circles are a very common shape in the consumer industry. Tin cans, glasses, tyres and CD's are a few examples of these.
+As a result you decide to add this information on your boxes: The largest (diameter) circular object that can fit into a given box.
+*/
+
+public class Polygon {
+    int sides;
+    int sideLength;
+
+    public Polygon(int sides, int sideLength) {
+        this.sides = sides;
+        this.sideLength = sideLength;
+    }
+    
+    public double circleDiameter(){
+        if(sides < 3){
+          throw new IllegalArgumentException("A polygon must have at least 3 sides.");
+        }
+        double diameter = (sideLength / ( 2 * Math.tan(Math.PI / sides)));
+        return 2  * diameter;
+    }
+    
+}
