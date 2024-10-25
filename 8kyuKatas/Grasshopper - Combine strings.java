a1bc5b12f1e5d552ff49af7dbe8c7b4b3279ffeb
Wiktor Gronostaj <wiktor.gronostaj03@gmail.com>
2024-10-25 12:46:51 +0200

    Grasshopper - Combine strings

---
+++ 8kyuKatas/Grasshopper - Combine strings.java
@@ -0,0 +1,14 @@
+/*
+Combine strings function
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+Example:
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+*/
+
+public class StringCombiner {
+    
+    public static String combineNames(String first,String last) {
+
+        return first + " " + last;
+    }
+}
