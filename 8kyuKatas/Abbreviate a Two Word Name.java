57d736ad32334892a1a56af73af6da68c2a82bc2
Wiktor Gronostaj <wiktor.gronostaj03@gmail.com>
2024-10-26 15:26:15 +0200

    Abbreviate a Two Word Name

---
+++ 8kyuKatas/Abbreviate a Two Word Name.java
@@ -0,0 +1,19 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+Sam Harris => S.H
+patrick feeney => P.F
+*/
+
+public class AbbreviateTwoWords {
+
+  public static String abbrevName(String name) {
+    String[] nameArr = name.split(" ");
+    String firstLetter = nameArr[0].toUpperCase();
+    String secondLetter = nameArr[1].toUpperCase();
+    char firstChar = firstLetter.charAt(0);
+    char secondChar = secondLetter.charAt(0);
+    return firstChar+"."+secondChar;
+  }
+}
