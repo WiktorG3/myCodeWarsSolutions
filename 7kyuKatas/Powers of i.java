40a8b04ab3bd2b62bb0052d335b411c5d26f7962
Wiktor Gronostaj <wiktor.gronostaj03@gmail.com>
2024-11-01 18:31:22 +0100

    Powers of i

---
+++ 7kyuKatas/Powers of i.java
@@ -0,0 +1,22 @@
+/*
+i is the imaginary unit, it is defined by 
+i²=−1, therefore it is a solution to 
+x²+1=0.
+Your Task
+Complete the function pofi that returns 
+i to the power of a given non-negative integer in its simplest form, as a string (answer may contain i).
+*/
+
+public class Kata {
+
+  public static String pofi(int n) {
+    switch(n % 4){
+        case 0: return "1";
+        case 1: return "i";
+        case 2: return "-1";
+        case 3: return "-i";
+        default: return "";
+    }
+  }
+    
+}
